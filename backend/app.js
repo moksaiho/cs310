66fc37d7f7d8cdf873dded30c086404f203e6173
mitester <49359325+mitester@users.noreply.github.com>
2023-12-07 09:19:55 +0800

    Socket io update for the backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { DBconnection } = require("./database");
 const app = express();
+const server = require('http').createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
 app.use(express.json());
 const port = 3000;
 console.log(DBconnection);
@@ -36,33 +39,42 @@ app.get("/messages", async (req, res) => {
     }
   });
 });
-//
-app.post("/messages", async (req, res) => {
-  2.18;
-  //   res.send("Hello, Express!");
-  dbConnection.connect();
-  console.log(req.body);
-  const { userid, content } = req.body;
-  let time = new Date().toISOString().replace("T", " ").substring(0, 19);
-  let sql = `
-  INSERT INTO messages (userid, timestamp, content) VALUES (?, ?, ?);
-  `;
 
-  dbConnection.query(sql, [userid, time, content], (err, results, _) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).send({ status: "fail", msg: err });
-    } else {
-      console.log("result是", results);
-      return res
-        .status(200)
-        .send({ status: "success", data: {}, msg: "insert success" });
-    }
-  });
-  // dbConnection.close();
-});
+io.on("connection", socket => {
+  /* 
+  Message Listener
+  Description: whenever received messages, we enter this callback. The function will 
+  (1) update the database
+  (2) broadcast the message to other client sockets
+  
+  Input:  msg
+  msg is a json file 
+  */
+  socket.on('message', msg => {
+    const {userid, content, image} = msg
+    let time = new Date().toISOString().replace("T", " ").substring(0, 19);
+    let sql = `
+    INSERT INTO messages (userid, timestamp, content, image) VALUES (?, ?, ?, ?);
+    `;
+
+    dbConnection.connect();
+
+    // Insert the message in database
+    dbConnection.query(sql, [userid, time, content], (err, results, _) => {
+      if (err) {
+        console.log(err);
+        socket.emit("error", "sql insertion error");
+      } else { // If insert sucessfully, broadcast the message to every user
+        console.log("result是", results);
+        io.emit('message', msg); // broadcast it to user
+      }
+    })
+
+    dbConnection.close();
+  })
+})
 
 // Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
